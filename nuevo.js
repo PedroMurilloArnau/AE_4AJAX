48374c2543b753efcbec5bd2b5b5e954df5e55c8
PedroMurilloArnau <Pedro.marnau@gmail.com>
2021-11-27 16:51:11 +0100

    modificando cosas

--- nuevo.js
+++ nuevo.js
@@ -8,10 +8,7 @@ const RECURSO = "ingredientes.json"
 
 let xmlHttp = new XMLHttpRequest()
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 30cea00b2be65429673cf0799d6aa06ff3fb7cd7
 xmlHttp.onreadystatechange = function(){
     if (this.readyState == 4){
         if (this.status ==200){
@@ -27,26 +24,17 @@ xmlHttp.onreadystatechange = function(){
 
 xmlHttp.open('GET', URL_DESTINO + RECURSO, true)
 xmlHttp.send()
-<<<<<<< HEAD
 
 }
 var tamaño
 var ingredientes
-=======
-} 
->>>>>>> 30cea00b2be65429673cf0799d6aa06ff3fb7cd7
 function leerRespuesta(jsonDoc){
 
     formi.innerHTML = ""
     
     var objetoJson = JSON.parse(jsonDoc)
-<<<<<<< HEAD
      ingredientes = objetoJson.PIZZERIA.INGREDIENTES;
      tamaño = objetoJson.PIZZERIA.TAMAÑO;  
-=======
-    var ingredientes = objetoJson.PIZZERIA.INGREDIENTES;
-    var tamaño = objetoJson.PIZZERIA.TAMAÑO;  
->>>>>>> 30cea00b2be65429673cf0799d6aa06ff3fb7cd7
        
     /*Recorro la lista pintando los distintos tamaños a elegir.*/
     
@@ -61,17 +49,11 @@ function leerRespuesta(jsonDoc){
         let input2 = document.createElement("input")
         input2.type = "radio"
         input2.id = tam.NOMBRE
-<<<<<<< HEAD
         input2.className = "tamaño"
         input2.name = "tamaño"
         input2.value = tam.VALUE
         formi.appendChild(input2)
         
-=======
-        input2.name = "tamaño"
-        input2.value = tam.VALUE
-        formi.appendChild(input2)
->>>>>>> 30cea00b2be65429673cf0799d6aa06ff3fb7cd7
     }
     let spa = document.createElement("br")
     formi.appendChild(spa)
@@ -84,16 +66,11 @@ function leerRespuesta(jsonDoc){
 
         let input1 = document.createElement("input")
         input1.type = "checkbox"
-<<<<<<< HEAD
         input1.className = "checkbox"
-=======
-        input1.class = "checkbox"
->>>>>>> 30cea00b2be65429673cf0799d6aa06ff3fb7cd7
         input1.id = ing.NOMBRE
         input1.name = ing.NOMBRE
         input1.value = "s"
         formi.appendChild(input1)
-<<<<<<< HEAD
         
         
         }
@@ -133,9 +110,3 @@ prec.innerHTML = "El precio total sera de " + total + " E."
 
 
 
-=======
-    
-        }
-        
-}
->>>>>>> 30cea00b2be65429673cf0799d6aa06ff3fb7cd7
